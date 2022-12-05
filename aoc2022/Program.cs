efc265dccaa32f6cc7e3006a80ac2afa9f565e9b
ttv-dev-spirit <ttv.dev.spirit@gmail.com>
2022-12-05 14:24:56 +0700

    day 5

--- aoc2022/Program.cs
+++ aoc2022/Program.cs
@@ -9,7 +9,7 @@ namespace aoc2022
         
         public static void Main(string[] args)
         {
-            new Day4().CalculatePart2(GetInput("day4_input"));
+            new Day5().CalculatePart2(GetInput("day5_input"));
         }
 
         private static string[] GetInput(string fileName)
